57431c500fca14a43ae0f11379f86a0d7421c8bc
giangblackk <giangblackk@gmail.com>
2024-09-01 22:36:48 +0700

    simplify data gen, run 60s then stop

--- src/streaming_benchmark/generators/simple_data_gen.py
+++ src/streaming_benchmark/generators/simple_data_gen.py
@@ -1,35 +1,45 @@
+import time
 import dbldatagen as dg
-from pyspark.sql.types import FloatType, IntegerType, StringType
+from pyspark.sql.types import IntegerType, StringType
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.remote("sc://localhost").getOrCreate()
+spark = (
+    SparkSession.builder.appName("simple_data_gen")
+    .remote("sc://localhost")
+    .getOrCreate()
+)
 
-row_count = 10 * 10
-column_count = 10
+row_count = 100
+row_per_sec = 1
+sec_to_run = 60
+
 testDataSpec = (
-    dg.DataGenerator(spark, name="test_data_set1", rows=row_count, partitions=4)
+    dg.DataGenerator(spark, name="test_data_set1", rows=row_count, partitions=1)
     .withIdOutput()
+    .withColumn("code1", IntegerType(), minValue=100, maxValue=200, random=True)
     .withColumn(
-        "r",
-        FloatType(),
-        expr="floor(rand() * 350) * (86400 + 3600)",
-        numColumns=column_count,
-    )
-    .withColumn("code1", IntegerType(), minValue=100, maxValue=200)
-    .withColumn("code2", "integer", minValue=0, maxValue=10, random=True)
-    .withColumn("code3", StringType(), values=["online", "offline", "unknown"])
-    .withColumn(
-        "code4", StringType(), values=["a", "b", "c"], random=True, percentNulls=0.05
-    )
-    .withColumn(
-        "code5", "string", values=["a", "b", "c"], random=True, weights=[9, 1, 1]
+        "code2",
+        StringType(),
+        values=["online", "offline", "unknown"],
+        percentNulls=0.05,
     )
 )
 
-dfTestData = testDataSpec.build()
+dfTestData = testDataSpec.build(
+    options={"rowsPerSecond": row_per_sec}, withStreaming=True
+)
 
 dfTestData.selectExpr(
-    "CAST(id AS STRING) as key", "CAST(code3 AS STRING) as value"
-).write.format("kafka").option("kafka.bootstrap.servers", "redpanda-0:9092").option(
-    "topic", "source-topic"
-).save()
+    "CAST(id as STRING) as key",
+    "CAST(CONCAT_WS('-', CAST(code1 as STRING), code2) AS STRING) as value",
+).writeStream.format("kafka").option(
+    "kafka.bootstrap.servers", "redpanda-0:9092"
+).option("topic", "source-topic").option(
+    "checkpointLocation", "/opt/spark/checkpoint"
+).start()
+
+time.sleep(sec_to_run)
+
+for s in spark.streams.active:
+    print(s)
+    s.stop()
