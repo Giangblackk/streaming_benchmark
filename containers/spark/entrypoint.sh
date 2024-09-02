5f698b16cdec8514255b27be20a548e5f6fc7687
giangblackk <giangblackk@gmail.com>
2024-09-02 13:38:00 +0700

    update iceberg and prepare deps using spark-sql

--- containers/spark/entrypoint.sh
+++ containers/spark/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Starting Spark Connect Server"
 
 bash /opt/spark/sbin/start-connect-server.sh \
-    --packages org.apache.spark:spark-connect_2.12:3.5.1,org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.5.2,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 \
+    --packages org.apache.spark:spark-connect_2.12:3.5.1,org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 \
     --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
     --conf spark.sql.catalog.spark_catalog=org.apache.iceberg.spark.SparkSessionCatalog \
     --conf spark.sql.catalog.spark_catalog.type=hive \
