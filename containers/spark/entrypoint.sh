03f316bc9027c8411a9cbf3aa9adccb29feb5d21
giangblackk <giangblackk@gmail.com>
2024-09-10 02:02:36 +0700

    add minio and nessi, update spark image to use nessie & s3

--- containers/spark/entrypoint.sh
+++ containers/spark/entrypoint.sh
@@ -4,14 +4,16 @@ echo "Starting Spark Connect Server"
 
 bash /opt/spark/sbin/start-connect-server.sh \
     --packages org.apache.spark:spark-connect_2.12:3.5.1,org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 \
-    --jars /opt/spark/jars/postgresql-42.7.4.jar \
-    --driver-class-path /opt/spark/jars/postgresql-42.7.4.jar \
+    --jars /opt/spark/jars/postgresql-42.7.4.jar,/opt/spark/jars/iceberg-aws-bundle-1.6.1.jar \
+    --driver-class-path /opt/spark/jars/postgresql-42.7.4.jar,/opt/spark/jars/iceberg-aws-bundle-1.6.1.jar \
     --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
-    --conf spark.sql.catalog.spark_catalog=org.apache.iceberg.spark.SparkSessionCatalog \
-    --conf spark.sql.catalog.spark_catalog.type=hive \
-    --conf spark.sql.catalog.local=org.apache.iceberg.spark.SparkCatalog \
-    --conf spark.sql.catalog.local.type=hadoop \
-    --conf spark.sql.catalog.local.warehouse=/opt/spark/warehouse \
-    --conf spark.sql.defaultCatalog=local
+    --conf spark.sql.catalog.nessie=org.apache.iceberg.spark.SparkCatalog \
+    --conf spark.sql.catalog.nessie.warehouse=s3://minio/warehouse \
+    --conf spark.sql.catalog.nessie.s3.endpoint=http://minio:9000 \
+    --conf spark.sql.catalog.nessie.catalog-impl=org.apache.iceberg.nessie.NessieCatalog \
+    --conf spark.sql.catalog.nessie.io-impl=org.apache.iceberg.aws.s3.S3FileIO \
+    --conf spark.sql.catalog.nessie.uri=http://nessie:19120/api/v1 \
+    --conf spark.sql.catalog.nessie.ref=main \
+    --conf spark.sql.catalog.nessie.cache-enabled=false
 
 sleep infinity
